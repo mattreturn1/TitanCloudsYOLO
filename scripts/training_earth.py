d75b345676695b82134a3315eea5bc419a6355ce
Francescus02 <118309292+Francescus02@users.noreply.github.com>
2025-06-03 12:15:26 +0200

    Update training_earth.py

--- scripts/training_earth.py
+++ scripts/training_earth.py
@@ -9,11 +9,11 @@ earth_model = YOLO('../yolo11n-seg.pt')
 # Start training (fine-tuning) the model on a new dataset
 earth_model.train(
     data='../yolo_configs/earth.yaml',     # Path to dataset configuration file (with classes, paths, etc.)
-    epochs=100,                            # Train for 100 epochs
+    epochs=150,                            # Train for 100 epochs
     imgsz=416,                             # Input image size (416x416)
     batch=16,                              # Batch size
     freeze=10,                             # Freeze the first 10 layers (useful for transfer learning)
-    patience=10,                           # Stop training early if no improvement after 10 epochs
+    patience=15,                           # Stop training early if no improvement after 10 epochs
     project='../runs/train',               # Directory where training results will be saved
     name='earth_clouds_yolo11n',           # Name of the training run (used for output folders)
     pretrained=True                        # Use pre-trained weights as a starting point
